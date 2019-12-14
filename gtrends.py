e3b7f9401620f186dd4fa8445f84711c75d8e520
Amulya <amulya@umich.edu>
2019-12-14 17:21:42 -0500

    adding the final touches on the gtrends db

--- gtrends.py
+++ gtrends.py
@@ -47,7 +47,8 @@ from pytrends.request import TrendReq
 pytrends = TrendReq(hl='en-US', tz=360)
 mean = []
 delta = []
-candidate_count = 0;
+candidate_count = 0
+
 for row in candidates:
   # print("full name: ", row[0])
   name = row[0]
@@ -61,6 +62,7 @@ for row in candidates:
   df4 = pytrends.interest_over_time()
   pytrends.build_payload([row[0]], cat=0, timeframe='today 5-y', geo='', gprop='')
   df5 = pytrends.interest_over_time()
+
   print('candidate: : ', row[0], " | 30 day mean: ", df[row[0]].mean() )
   print('candidate: : ', row[0], " | 90 day mean: ", df2[row[0]].mean() )
   print('candidate: : ', row[0], " | 1 day  mean: ", df3[row[0]].mean() )
@@ -68,15 +70,17 @@ for row in candidates:
   print('candidate: : ', row[0], " | 5 yr mean: ", df5[row[0]].mean() )
   cur.execute("INSERT INTO Gtrend_MEAN (id, name, onemonth, threemonth, oneday, sevenday, fiveyear) VALUES (?, ?, ? ,?,?, ?, ?)", (candidate_count, name, df[row[0]].mean(), df2[row[0]].mean(), df3[row[0]].mean(), df4[row[0]].mean(), df5[row[0]].mean() ))
 
-  print('candidate: : ', row[0], " | 30 day delta: ", df.iloc[:,0][-1] - df.iloc[:,0][0] )
-  print('candidate: : ', row[0], " | 90 day delta: ", df2.iloc[:,0][-1] - df2.iloc[:,0][0])
-  print('candidate: : ', row[0], " | 1 day delta: ", df3.iloc[:,0][-1] - df3.iloc[:,0][0] )
-  print('candidate: : ', row[0], " | 7 day delta: ", df4.iloc[:,0][-1] - df4.iloc[:,0][0] )
-  print('candidate: : ', row[0], " | 5 yr delta: ", df5.iloc[:,0][-1] - df5.iloc[:,0][0] )
-  cur.execute("INSERT INTO Gtrend_DELTA (id, name, onemonth, threemonth, oneday, sevenday, fiveyear) VALUES (?, ?, ? ,?,?, ?, ?)", (candidate_count, name,  df.iloc[:,0][-1] - df.iloc[:,0][0],  df2.iloc[:,0][-1] - df2.iloc[:,0][0],  df3.iloc[:,0][-1] - df3.iloc[:,0][0],  df4.iloc[:,0][-1] - df4.iloc[:,0][0],  df5.iloc[:,0][-1] - df5.iloc[:,0][0] ))
+  print('candidate: : ', row[0], " | 30 day delta: ",  df[row[0]][-1] - df[row[0]][0] )
+  print('candidate: : ', row[0], " | 90 day delta: ",  df2[row[0]][-1] - df2[row[0]][0])
+  print('candidate: : ', row[0], " | 1 day delta: ", df3[row[0]][-1] - df3[row[0]][0] )
+  print('candidate: : ', row[0], " | 7 day delta: ", df4[row[0]][-1] - df4[row[0]][0] )
+  print('candidate: : ', row[0], " | 5 yr delta: ", df5[row[0]][-1] - df5[row[0]][0] )
+
+  cur.execute("INSERT INTO Gtrend_DELTA (id, name, onemonth, threemonth, oneday, sevenday, fiveyear) VALUES (?, ?, ? ,?,?, ?, ?)", (candidate_count, name,  float(df.iloc[:,0][-1] - df.iloc[:,0][0]),  float(df2.iloc[:,0][-1] - df2.iloc[:,0][0]),  float(df3.iloc[:,0][-1] - df3.iloc[:,0][0]), float(df4.iloc[:,0][-1] - df4.iloc[:,0][0]),  float(df5.iloc[:,0][-1] - df5.iloc[:,0][0]) ))
 
   candidate_count += 1
   #mean.append(df[row[0]].mean())
   # print('delta: ', df.iloc[:,0][-1] - df.iloc[:,0][0])
   #delta.append(df.iloc[:,0][-1] - df.iloc[:,0][0])
 
+conn.commit()
