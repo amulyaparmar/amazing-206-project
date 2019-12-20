2d5c555a1e1198d6d359bb98bf29bb5f0c5ae25e
elizaho <elizadh@umich.edu>
2019-12-19 19:17:02 -0500

    Consolidate master and sitetrafficapi.py

--- si206_final.py
+++ si206_final.py
@@ -133,8 +133,6 @@ def read_cache(CACHE_FNAME):
 import requests
 
 def get_sitetraffic(candidates, cur, conn):
-  cur.execute("DROP TABLE IF EXISTS WebsiteData")
-  cur.execute("DROP TABLE IF EXISTS WebsiteDelta")
   cur.execute("CREATE TABLE WebsiteData (id INT PRIMARY KEY, candidate TEXT, category TEXT, score REAL)")
   cur.execute("CREATE TABLE WebsiteDelta (id INT PRIMARY KEY, candidate TEXT, category TEXT, score REAL)")
     # api_c_code  = country_code      # country code (e.g. "USA", "USA;CAN")
@@ -151,32 +149,32 @@ def get_sitetraffic(candidates, cur, conn):
     print(candidate[0])
     score = data['data']['estimations']['visitors']['daily']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Daily Visitors', score))
       data_id += 1
     score = data['data']['estimations']['visitors']['monthly']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Monthly Visitors', score))
       data_id += 1
     score = data['data']['estimations']['visitors']['yearly']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Yearly Visitors', score))
       data_id += 1
     score = data['data']['estimations']['pageviews']['daily']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Daily Pageviews', score))
       data_id += 1
     score = data['data']['estimations']['pageviews']['monthly']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Monthly Pageviews', score))
       data_id += 1
     score = data['data']['estimations']['pageviews']['yearly']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Yearly Pageviews', score))
       data_id += 1
     score = data['data']['alexa']['rank']['3_months']
@@ -197,22 +195,22 @@ def get_sitetraffic(candidates, cur, conn):
       data_id += 1
     score = data['data']['alexa']['pageviews']['3_months']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Pageviews Over 3 Months', score))
       data_id += 1
     score = data['data']['alexa']['pageviews']['1_month']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Pageviews Over 1 Month', score))
       data_id += 1
     score = data['data']['alexa']['pageviews']['7_days']
     if (score != None):
-      score = int(score.replace(',',''))
+      score = float(score.replace(',',''))
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Pageviews Over 1 Week', score))
       data_id += 1
     score = data['data']['alexa']['pageviews']['1_day']
     if (score != None):
-      score = int(score.replace(',',''))    
+      score = float(score.replace(',',''))    
       cur.execute("INSERT INTO WebsiteData (id, candidate, category, score) VALUES (?,?,?,?)", (data_id, candidate[0], 'Pageviews Over 1 Day', score))
       data_id += 1
     score = data['data']['alexa']['reach']['3_months']
@@ -259,7 +257,7 @@ def get_sitetraffic(candidates, cur, conn):
     cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Pageviews Over 1 Day', score))
     delta_id += 1    
     score = data['data']['alexa']['reach_delta']['3_months']
-    cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Reacb Over 3 Months', score))
+    cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Reach Over 3 Months', score))
     delta_id += 1
     score = data['data']['alexa']['reach_delta']['1_month']
     cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Reach Over 1 Month', score))
@@ -268,7 +266,7 @@ def get_sitetraffic(candidates, cur, conn):
     cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Reach Over 1 Week', score))
     delta_id += 1
     score = data['data']['alexa']['reach_delta']['1_day']
-    cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Recah Over 1 Day', score))
+    cur.execute("INSERT INTO WebsiteDelta (id, candidate, category, score) VALUES (?,?,?,?)", (delta_id, candidate[0], 'Reach Over 1 Day', score))
     delta_id += 1
   conn.commit()
 
